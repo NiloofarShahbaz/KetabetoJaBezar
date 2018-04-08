8689623de9f9f9aec7c56379d76d0eafb6f1b6cc
Niloofar <niloofar1376@gmail.com>
2018-04-08 20:26:10 +0430

    bye ubuntu !

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,6 @@ from . import views
 #app_name='books'
 
 urlpatterns = [
-    path('',views.BookListView.as_view(), name='books'),
+    path('',views.BookListView.as_view(), name='booklist'),
     path('<uuid:pk>/', view=views.BookDetailView.as_view(), name='bookdetail'),
 ]
--- books/views.py
+++ books/views.py
@@ -4,7 +4,7 @@ from .models import Book,User_Book
 from django.views import generic
 
 class BookListView(generic.ListView):
-    template_name = 'pages/books.html'
+    template_name = 'pages/BookListPage.html'
     context_object_name = 'user_book'
     def get_queryset(self):
         return User_Book.objects.order_by('release_date').reverse()
@@ -12,4 +12,4 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = User_Book
-    template_name = 'pages/bookdetail.html'
+    template_name = 'pages/BookDetails.html'
