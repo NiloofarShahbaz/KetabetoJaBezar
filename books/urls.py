78ff67b8f0cbcba99cd4ce4523892272e6894ce3
Niloofar <niloofar1376@gmail.com>
2018-04-22 18:21:42 +0430

    search page added

--- books/urls.py
+++ books/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('new/',views.BookList_NewRealesed.as_view(), name='booklist_newreleased'),
     path('alpha/',views.BookList_Alphabetical.as_view(), name='booklist_alphabetical'),
     path('<uuid:pk>/', view=views.BookDetailView.as_view(), name='bookdetail'),
+    path('search/',views.BookSearch.as_view(),name='search'),
+    path('search/searchby',views.BookSearchBy.as_view(),name='search_by'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 from django.shortcuts import render
+from django.contrib.postgres.search import SearchVector
 from .models import Book,User_Book
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,TemplateView
 
 
 
@@ -28,3 +29,17 @@ class BookList_NewRealesed(ListView):
 class BookDetailView(DetailView):
     model = User_Book
     template_name = 'pages/BookDetails.html'
+
+class BookSearch(TemplateView):
+    template_name='pages/SearchPage.html'
+
+class BookSearchBy(ListView):
+    template_name = 'pages/SearchPage.html'
+    context_object_name = 'result'
+
+    def get_queryset(self):
+        field=self.request.GET.get('field')
+        if field:
+            return User_Book.objects.order_by('book')\
+                .annotate(search=SearchVector('book__book_name','book__book_author','address'))\
+                .filter(search=field)
